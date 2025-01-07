232d48c6146a9ff4d8649a0d74e4345cf20bd395
Uniqueideass <uniqueideas85@gmail.com>
2025-01-07 19:17:56 +0000

    Initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				'RecklessNeueBold':['RecklessNeue Bold'],
+				'RecklessNeueMedium':['RecklessNeue Medium'],
+				'RecklessNeueMediumItalic':['RecklessNeue Medium Italic'],
+				'RecklessNeueRegular':['RecklessNeue Regular'],
+				'RecklessNeueRegularItalic':['RecklessNeue Regular Italic'],
+				'RecklessNeueSemiBold':['RecklessNeue SemiBold'],
+				'RecklessNeueSemiBoldItalic':['RecklessNeue SemiBold Italic'],
+				'RecklessNeueThin':['RecklessNeue Thin'],
+				'RecklessNeueThinItalic':['RecklessNeue Thin Italic'],
+				'CentraNo2-BlackItalic': ['CentraNo2 Black Italic'],
+				'CentraNo2-Black': ['CentraNo2 Black'],
+				'CentraNo2-Bold': ['CentraNo2 Bold'],
+				'CentraNo2-Bolditalic': ['CentraNo2 Bold Italic'],
+				'CentraNo2-Book': ['CentraNo2 Book'],
+				'CentraNo2-BookItalic': ['CentraNo2 Book Italic'],
+				'CentraNo2-Extrabold': ['CentraNo2 Extrabold'],
+				'CentraNo2-ExtraboldItalic': ['CentraNo2 Extrabold Italic'],
+				'CentraNo2-Hairline': ['CentraNo2 Hairline'],
+				'CentraNo2-HairlineItalic': ['CentraNo2 Hairline Italic'],
+				'CentraNo2-Light': ['CentraNo2 Light'],
+				'CentraNo2-LightItalic': ['CentraNo2 Light Italic'],
+				'CentraNo2-Medium': ['CentraNo2 Medium'],
+				'CentraNo2-MediumItalic': ['CentraNo2 Medium Italic'],
+				'CentraNo2-Thin': ['CentraNo2 Thin'],
+				'CentraNo2-ThinItalic': ['CentraNo2 Thin Italic'],
+				
+			},
+		},
+	},
+	plugins: [],
+}
